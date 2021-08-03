1a0e30905f2f284f8394e5c28c5e19c797a61e1e
Gui Barscevicius <guilbrs@gmail.com>
2021-08-03 14:37:44 -0300

    typo

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,7 +7,7 @@
       logo="https://i.pinimg.com/originals/a0/db/2f/a0db2ff946b49086a105a2722db66d22.png"
       brand="The Guardian"
       :title="(i % 3)
-        ? 'Exemplo de cardinho de artigo, esse artigo tem ima imagem, e ela aparece aqui.'
+        ? 'Exemplo de cardinho de artigo. Esse artigo tem uma imagem, e ela aparece aqui.'
         : 'Exemplo de cardinho sem imagem. Nesse caso, o logo da fonte (The Guardian) aparece;'
       "
       description="Seguindo a ideia de que o cardinho tá bão mas tá ruim, podemos concluir q sempre tem como piorar. Ou melhorar"
