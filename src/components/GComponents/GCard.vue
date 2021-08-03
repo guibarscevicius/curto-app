b79457a34fd37d0c1134d0bd2c415cbaa375e643
Gui Barscevicius <guilbrs@gmail.com>
2021-08-03 14:36:32 -0300

    fix card border

--- src/components/GComponents/GCard.vue
+++ src/components/GComponents/GCard.vue
@@ -4,13 +4,9 @@
       overflow-hidden
       rounded-lg
     "
-    :class="active ? [`
-      border-green-200 dark:border-green-800
-      bg-green-100 dark:bg-green-900
-    `] : [`
-      bg-gray-100 dark:bg-gray-800
-      border-2 border-gray-100 dark:border-gray-800
-    `]"
+    :class="active
+      ? ['bg-green-100 dark:bg-green-900']
+      : ['bg-gray-100 dark:bg-gray-800']"
   >
     <slot />
   </div>
