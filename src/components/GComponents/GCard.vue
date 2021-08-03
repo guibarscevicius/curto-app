bfb97a3cf2ad1da22c36863d4c26cccad17a45d6
Gui Barscevicius <guilbrs@gmail.com>
2021-08-03 07:27:22 -0300

    card component

--- src/components/GComponents/GCard.vue
+++ src/components/GComponents/GCard.vue
@@ -3,9 +3,26 @@
     class="
       overflow-hidden
       rounded-lg
-    bg-gray-100 dark:bg-gray-800
+      border-box border-2
     "
+    :class="active ? [`
+      border-green-200 dark:border-green-800
+      bg-green-100 dark:bg-green-900
+    `] : [`
+      bg-gray-100 dark:bg-gray-800
+      border-2 border-gray-100 dark:border-gray-800
+    `]"
   >
     <slot />
   </div>
 </template>
+
+<script>
+export default {
+  props: { active: Boolean },
+
+  setup(props) {
+    return { ...props }
+  },
+}
+</script>
