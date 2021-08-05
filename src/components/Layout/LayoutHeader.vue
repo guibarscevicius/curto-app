bf659c87cfe2479e0899f81663bd8e5fe22d263a
Gui Barscevicius <guilbrs@gmail.com>
2021-08-05 19:46:27 -0300

    reorganize stuff

---
+++ src/components/Layout/LayoutHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-row justify-between">
+    <g-branded-logo />
+
+    <div class="actions">
+      <a
+        class="icon-btn mx-2 .space-x-0.5"
+        :title="t('button.toggle_langs')"
+        @click="toggleLocales"
+      >
+        <carbon-language />
+      </a>
+
+      <button
+        class="icon-btn mx-2 !outline-none"
+        :title="t('button.toggle_dark')"
+        @click="toggleDark"
+      >
+        <carbon-moon v-if="isDark" />
+        <carbon-sun v-else />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useI18n } from 'vue-i18n'
+import { isDark, toggleDark } from '~/logic'
+
+export default {
+  setup() {
+    const { t, availableLocales, locale } = useI18n()
+
+    const toggleLocales = () => {
+      const locales = availableLocales
+
+      locale.value = locales[
+        (locales.indexOf(locale.value) + 1)
+        % locales.length
+      ]
+    }
+
+    return {
+      t,
+      isDark,
+      toggleDark,
+      toggleLocales,
+    }
+  },
+}
+</script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,28 +1,7 @@
 <template>
-  <app-layout>
+  <g-app-layout>
     <template #header>
-      <div class="flex flex-row justify-between">
-        <g-branded-logo />
-
-        <div class="actions">
-          <a
-            class="icon-btn mx-2 .space-x-0.5"
-            :title="t('button.toggle_langs')"
-            @click="toggleLocales"
-          >
-            <carbon-language />
-          </a>
-
-          <button
-            class="icon-btn mx-2 !outline-none"
-            :title="t('button.toggle_dark')"
-            @click="toggleDark"
-          >
-            <carbon-moon v-if="isDark" />
-            <carbon-sun v-else />
-          </button>
-        </div>
-      </div>
+      <layout-header />
     </template>
 
     <router-view />
@@ -34,34 +13,15 @@
     <template #additional>
       <g-player />
     </template>
-  </app-layout>
+  </g-app-layout>
 </template>
 
 <script>
-import { useI18n } from 'vue-i18n'
-import { isDark, toggleDark } from '~/logic'
 import { menuItems } from '~/content'
 
 export default {
   setup() {
-    const { t, availableLocales, locale } = useI18n()
-
-    const toggleLocales = () => {
-      const locales = availableLocales
-
-      locale.value = locales[
-        (locales.indexOf(locale.value) + 1)
-        % locales.length
-      ]
-    }
-
-    return {
-      isDark,
-      toggleDark,
-      t,
-      toggleLocales,
-      menuItems,
-    }
+    return { menuItems }
   },
 }
 </script>
