dc8003e4c24142e163a842592096943d33d38f6d
Gui Barscevicius <guilbrs@gmail.com>
2021-08-05 11:29:43 -0300

    route transitions

--- src/components/GComponents/AppLayout.vue
+++ src/components/GComponents/AppLayout.vue
@@ -31,7 +31,13 @@
 
       <div class="mb-16 h-full">
         <div class="main-content pt-4 px-2 h-full overflow-y-auto" :style="{ height }">
-          <slot />
+          <div class="relative">
+            <router-view v-slot="{ Component }">
+              <transition name="fade">
+                <component :is="Component" class="absolute top-0 left-0" />
+              </transition>
+            </router-view>
+          </div>
         </div>
       </div>
     </div>
