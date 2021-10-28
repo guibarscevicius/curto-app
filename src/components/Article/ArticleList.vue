4dc83d1b3499fbfcbb6a151c8945d30ef8af2e25
Gui Barscevicius <guilbrs@gmail.com>
2021-10-28 10:45:38 -0300

    adapt article store to the new api

--- src/components/Article/ArticleList.vue
+++ src/components/Article/ArticleList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="article-list grid gap-2">
     <article-card
-      v-for="({ _id, media, title, description }) in article.list.value"
+      v-for="({ _id, media, title, description }) in article.listArticles()"
       :key="_id"
       :image="media"
       logo="https://i.pinimg.com/originals/a0/db/2f/a0db2ff946b49086a105a2722db66d22.png"
--- src/modules/article.js
+++ src/modules/article.js
@@ -1,6 +1,8 @@
 import { reactive, toRefs } from 'vue'
 import request from '~/modules/request'
 
+const baseUrl = import.meta.env.VITE_API_URL
+
 const state = reactive({
   list: [],
   selected: [],
@@ -9,11 +11,23 @@ const state = reactive({
 
 export default function useArticles() {
   const fetchArticles = async () => {
-    const [err, articles] = await request.get('/article/list')
+    const [err, articles] = await request.get('/articles')
     if (err) return
 
     state.list = articles
   }
+
+  const listArticles = () => state.list
+    .map(article => ({
+      ...article,
+      media: baseUrl + (article.media?.formats.thumbnail.url
+        || article.media?.url),
+      source: {
+        ...article.source,
+        logo: baseUrl + (article.source?.logo.formats.thumbnail.url
+          || article.source?.logo.url),
+      },
+    }))
 
   const addSelected = (id) => {
     state.selected = [...new Set([...state.selected, id])]
@@ -23,12 +37,15 @@ export default function useArticles() {
     state.selected = state.selected.filter(selected => selected !== id)
   }
 
-  const listSelected = () => state.selected
-    .map(id => state.list.find(({ _id }) => _id === id))
+  function listSelected() {
+    return state.selected.map(id =>
+      this.listArticles().find(({ _id }) => _id === id))
+  }
 
   return {
     ...toRefs(state),
     fetchArticles,
+    listArticles,
     addSelected,
     removeSelected,
     listSelected,
