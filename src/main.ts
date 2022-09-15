0f2783d334f53eba0a0297995be7cc89f6dea7da
Gui Barscevicius <guilbrs@gmail.com>
2022-09-15 16:20:33 -0300

    track components via sentry

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,8 @@ export const createApp = ViteSSG(
       // Set tracesSampleRate to 1.0 to capture 100%
       // of transactions for performance monitoring.
       // We recommend adjusting this value in production
-      tracesSampleRate: 1.0,
+      tracesSampleRate: 0.2,
+      trackComponents: true,
     })
 
     // install all modules under `modules/`
